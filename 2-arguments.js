46379a61907e4e4f13bb735ab753faf0a79c40f1
alindafortunate <alindafortunate5@gmail.com>
2025-07-11 11:45:20 +0300

    Edited something in the task.

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,9 +1,9 @@
 let result = ['No argument', 'Argument found', 'Arguments found']
 
 if (result[0] === process.argv[0]) {
-    console.log('No argument')
-} else if (result[1] === process.argv[1]) {
     console.log('Argument found')
+} else if (result[1] === process.argv[1]) {
+    console.log('Arguments found')
 } else {
     console.log('Arguments found')
 }
