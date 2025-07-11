3acf735d98e4bec67dfa4b25a26fcac8f0996f03
alindafortunate <alindafortunate5@gmail.com>
2025-07-11 12:26:39 +0300

    Completed the task.

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,15 +1,14 @@
 let result = ['No argument', 'Argument found', 'Arguments found']
 
-if (process.argv[2]) {
-    console.log(result[1])
-}
-else if (process.argv[3]) {
+if (process.argv[3]) {
+
     console.log(result[2])
-}
-else if (process.argv[2]) {
-    console.log('Arguments found')
-}
-else {
+
+} else if (process.argv[2]) {
+
+    console.log(result[1])
+
+} else {
     console.log(result[0])
 
 }
