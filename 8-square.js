27c8c4dee54d074065b912ad5e3c2099b5daf0f5
alindafortunate <alindafortunate5@gmail.com>
2025-07-11 14:07:40 +0300

    Initial commit for task 8.

---
+++ 8-square.js
@@ -0,0 +1,12 @@
+const process = require('process')
+let x = process.argv[2]
+let value = 'X'
+if (x / 1) {
+    for (i = 0; i < x; i++) {
+        if (i < x) {
+            console.log(value)
+        }
+    }
+} else {
+    console.log('Missing size')
+}
