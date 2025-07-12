441569ebc0d6f661d5d3e4e5482621914a50b2d0
alindafortunate <alindafortunate5@gmail.com>
2025-07-12 17:34:52 +0300

    Completed task 8 though yet to understand.

--- 8-square.js
+++ 8-square.js
@@ -1,12 +1,25 @@
+// Write a script that prints a square
+
+// The first argument is the size of the square
+// If the first argument can’t be converted to an integer, print “Missing size”
+// You must use the character X to print the square
+// You must use console.log(...) to print all output
+// You are not allowed to use var
+// You must use a loop (while, for, etc.)
+
 const process = require('process')
 let x = process.argv[2]
-let value = 'X'
+let value = ''
+
 if (x / 1) {
     for (i = 0; i < x; i++) {
-        if (i < x) {
-            console.log(value++)
-        }
+        value += "X"
+
     }
+    for (j = 0; j < x; j++) {
+        console.log(value)
+    }
+
 } else {
     console.log('Missing size')
 }
